38a34b7fa343be8d73d6c471160e0a2066a3794d
Björn Henriksson <bjarna2@gmail.com>
2014-11-12 16:45:27 +0100

    added templates

--- get_class/views.py
+++ get_class/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from get_class.models import Courses
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    get_url_list = Courses.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('get_class/index.html')
+    context = RequestContext(request, {
+        'get_url_list': get_url_list,
+    })
+    return HttpResponse(template.render(context))
