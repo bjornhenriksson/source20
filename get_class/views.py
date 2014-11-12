82c5386560997f9dceb3c5fde9d05b4b50ef6073
Björn Henriksson <bjarna2@gmail.com>
2014-11-12 17:20:30 +0100

    added pagination

--- get_class/views.py
+++ get_class/views.py
@@ -4,7 +4,9 @@ from django.template import RequestContext, loader
 from get_class.models import Courses
 
 def index(request):
-    get_url_list = Courses.objects.order_by('-pub_date')[:5]
+    page = request.GET.get('page')
+    current_course = request.GET.get('course')
+    get_url_list = Courses.objects.filter(course_name=current_course, id=page)
     template = loader.get_template('get_class/index.html')
     context = RequestContext(request, {
         'get_url_list': get_url_list,
