2a28322841aaca4d0bf9c63ffd3c6517a1df5819
Björn Henriksson <bjarna2@gmail.com>
2014-11-18 15:22:26 +0100

    changed logic for order

--- get_class/views.py
+++ get_class/views.py
@@ -4,56 +4,47 @@ from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from get_class.models import GetUrl,Course
 from django.contrib.auth import logout
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/login/')
 
-    page = request.GET.get('page')
-    if not page:
-        page = 1
     current_course = request.GET.get('course')
     get_course_id = Course.objects.filter(course_name=current_course)
-    get_url_list = GetUrl.objects.filter(current_course=get_course_id, id=page)
-    get_previous_id = int(page) - 1
-    get_next_id = int(page) + 1
-    if GetUrl.objects.filter(id=get_next_id).exists():
-        next_page = '<a class="pagination next active"><i class="fa fa-arrow-right"></i></a>'
-    else:
-        next_page = '<a class="pagination next grey"><i class="fa fa-arrow-right"></i></a>'
-    if GetUrl.objects.filter(id=get_previous_id).exists():
-        prev_page = '<a href="?course=%s&page=%s" class="pagination active"><i class="fa fa-arrow-left"></i></a>' % (current_course, get_previous_id)
-    else:
-        prev_page = '<a class="pagination grey"><i class="fa fa-arrow-left"></i></a>'
+    get_url_list = GetUrl.objects.filter(current_course=get_course_id).order_by('-votes')
+    paginator = Paginator(get_url_list, 1)
+    page = request.GET.get('page')
 
-    ratio = GetUrl.objects.filter(current_course=get_course_id, id=page)
+    try:
+        urls = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        urls = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        urls = paginator.page(paginator.num_pages)
 
-    context = {
-        'get_url_list': get_url_list,
-        'prev_page': prev_page,
-        'next_page': next_page,
-        'current_ratio': ratio,
-        'current_user': request.user,
-    }
-
-    return render(request, 'get_class/index.html', context)
+    return render(request, 'get_class/index.html', {"urls": urls, "course": current_course, 'current_user': request.user})
 
 def vote_up(request):
+     id = request.GET.get('id')
      page = request.GET.get('page')
      current_course = request.GET.get('course')
      get_course_id = Course.objects.filter(course_name=current_course)
-     find_url = GetUrl.objects.get(current_course=get_course_id, id=page)
+     find_url = GetUrl.objects.get(current_course=get_course_id, id=id)
      find_url.votes += 1
      find_url.save()
      next_page = int(page) + 1
      return HttpResponseRedirect('/get_class/?course=%s&page=%s' % (current_course, next_page))
 
 def vote_down(request):
+     id = request.GET.get('id')
      page = request.GET.get('page')
      current_course = request.GET.get('course')
      get_course_id = Course.objects.filter(course_name=current_course)
-     find_url = GetUrl.objects.get(current_course=get_course_id, id=page)
+     find_url = GetUrl.objects.get(current_course=get_course_id, id=id)
      find_url.votes -= 1
      find_url.save()
      next_page = int(page) + 1
